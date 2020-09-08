ff78a2a9682b87069adc7107edb086166daed5c7
nicole <nicole.m.dreon@gmail.com>
2020-09-08 13:38:23 -0400

    basic skelton, all prompts added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var yeild = 50;
+
+prompt("How many inches of rain fell?");
+
+prompt("Did you use fertilizer?");
+
+prompt("Did you use premium or regular fertilizer?");
+
+console.log("The yeild should be " + yeild + " bushels per acre.");
