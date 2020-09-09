495616befffd37b9505e6fb5d67b800d4bfdc807
nicole <nicole.m.dreon@gmail.com>
2020-09-09 11:11:27 -0400

    rainfall conversion

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 var yeild = 50;
 var rain = "*";
+var output = x;
+var x = yeild;
 
 var rainfall = prompt("How many inches of rain fell?");
 console.log(rain.repeat(rainfall));
+if (rainfall >= 20) {
+  output = yeild * 0.9;
+} else if (rainfall < 10) {
+  output = yeild * 0.8;
+} else {
+  output = yeild;
+}
 var fertilizer = prompt("Did you use fertilizer?");
 
 var type = prompt("Did you use premium or regular fertilizer?");
 //if ((type = premium)) {
 //console.log(yeild * 1.15);
 //}
-console.log("The yeild should be " + yeild + " bushels per acre.");
-if (rain < 10) {
-  yeild * 0.8;
-}
+console.log("The yeild should be " + output + " bushels per acre.");
