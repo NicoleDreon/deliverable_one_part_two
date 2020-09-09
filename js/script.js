b961e3f45bbfcddf895300e410c24009f6089f6e
nicole <nicole.m.dreon@gmail.com>
2020-09-09 10:34:57 -0400

    rain imput conver to *(s) in console log

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 var yeild = 50;
+var rain = "*";
 
-prompt("How many inches of rain fell?");
+var rainfall = prompt("How many inches of rain fell?");
+console.log(rain.repeat(rainfall));
+var fertilizer = prompt("Did you use fertilizer?");
 
-prompt("Did you use fertilizer?");
-
-prompt("Did you use premium or regular fertilizer?");
-
+var type = prompt("Did you use premium or regular fertilizer?");
+//if ((type = premium)) {
+//console.log(yeild * 1.15);
+//}
 console.log("The yeild should be " + yeild + " bushels per acre.");
+if (rain < 10) {
+  yeild * 0.8;
+}
